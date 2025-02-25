7da7c4d43b1d681bd332ee9f8cd4fd1823292992
mukesh vantakula <durgasaimukeshvantakula5764@gmail.com>
2025-02-25 10:24:18 +0530

    added schema and database

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 const express = require('express');
 const { resolve } = require('path');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
 
-// Load environment variables from .env file
-dotenv.config();
+
+//Connecting Mongodb
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGO_URI,{}).then(()=>console.log(`Connected to database`))
+.catch((error)=>{console.error('error connecting to database', error)});
+
 
 const app = express();
-const port = process.env.PORT || 3010;
+const port = 3010;
+
+// Schema 
+app.use(express.json());
+const User = require('./schema');
+
+app.post('/api/users', async(req,res)=>{
+  try{
+    // console.log(req.body);
+    const {name, email, password, createdAt} = req.body;
+
+    if (!name || !email || !password){
+      return res.status(400).json({message:'Validation error: All fields are required'});
+    }
+
+    const newUser = new User({name, email, password, createdAt});
+    await newUser.save();
+    res.status(201).json({messages:"User created successfully"});
+
+    const userExist = User.findOne({email: email});
+
+    if(userExist){
+      return res.status(400).json({msg:"Email already exists"})
+    }
+
+    
+  }catch(err){
+    console.error('Error saving user:',err);
+    res.status(500).json({message:'server error'});
+  }
+})
 
 app.use(express.static('static'));
 
@@ -15,14 +49,6 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to database'))
-.catch((error) => console.error(`Error connecting to database: ${error}`));
-
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
